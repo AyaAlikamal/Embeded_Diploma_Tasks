ecdb55881f44aa62b070ae6bfbf3f058d361d3f3
aya <aaya42727@gmail.com>
2023-06-29 16:52:36 +0300

    uploading it with Function

--- Prime_Check/Prime_Check.c
+++ Prime_Check/Prime_Check.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
  
 unsigned int Number;
-
+void Prime_Cheker(unsigned int Input);
 int main(){
 printf("Enter the Number:");
-scanf(" %i", &Number);    
-if((Number % 2)){
+scanf(" %i", &Number); 
+Prime_Cheker(Number);
+
+}
+
+void Prime_Cheker(unsigned int Input){
+    if((Number % 2)){
 if((Number % 3)){
     if((Number % Number)==0){
         printf("The Number is Prime.");
